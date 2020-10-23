1b2813e1a1d3de8ad960c920bf9f0bd2c2626aa5
Alex Lazar <lazarva25@gmail.com>
2020-10-23 16:55:11 +0300

    api keys for firebase

---
+++ constant/firebaseKeys.js
@@ -0,0 +1,13 @@
+export default{
+    firebaseConfig: {
+        apiKey: 'AIzaSyDED3NoPmy47xVIHlJM6R_leBhOTq3kfgw',
+        authDomain: 'msa-app-46bf5.firebaseapp.com',
+        databaseURL: 'https://msa-app-46bf5.firebaseio.com',
+        projectId: 'msa-app-46bf5',
+        storageBucket: 'msa-app-46bf5.appspot.com',
+        messagingSenderId: '266924934297',
+        appId: '1:266924934297:web:e42eff6694b34abff36408',
+        measurementId: 'G-EY5QZ5072M',
+    }
+  };
+  
