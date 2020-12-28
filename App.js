63f986c32c77e706defbb2d59c7bba04a8dbbc1a
Alex Lazar <lazarva25@gmail.com>
2020-12-28 16:52:46 +0200

    navigation from auth implemented

--- App.js
+++ App.js
@@ -1,17 +1,10 @@
-import "react-native-gesture-handler";
-import { StatusBar } from "expo-status-bar";
 import React from "react";
+import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import Home from "./screens/home/Home";
-import Signup from "./screens/auth/Signup";
-
-import * as firebase from "firebase";
-import firebaseKeys from "./constant/firebaseKeys";
-
-if (!firebase.app.length) {
-  firebase.initializeApp(firebaseKeys.firebaseConfig);
-}
+import LoginPage from "./screens/auth/Login";
+import SignupPage from "./screens/auth/Signup";
+import Home from "./screens/home";
 
 const Stack = createStackNavigator();
 
@@ -19,17 +12,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen name="Signup" component={SignupPage} />
+        <Stack.Screen name="Login" component={LoginPage} />
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
