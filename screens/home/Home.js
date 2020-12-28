636fe929434add2175f794a6b7c4561f60435fe9
Alex Lazar <lazarva25@gmail.com>
2020-12-28 16:54:56 +0200

    minor changes

--- screens/home/Home.js
+++ screens/home/Home.js
@@ -5,7 +5,6 @@ export default function Home({ navigation }) {
   return (
     <View style={styles.container}>
       <Text>Home</Text>
-      <Button title="Go back" onPress={() => navigation.navigate("Login")} />
     </View>
   );
 }
