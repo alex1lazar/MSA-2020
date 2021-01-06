093c2c6c26eb93bdc95580de5f56c9189b273ede
Topliceanu Razvan <topliceanurazvan98@gmail.com>
2021-01-06 20:35:47 +0200

    Timer pause/play/cancel

--- screens/focus/Focus.js
+++ screens/focus/Focus.js
@@ -1,20 +1,19 @@
 import React, { useEffect, useRef, useState } from "react";
 import {
-  FlatList,
-  SectionList,
-  SafeAreaView,
   StyleSheet,
   Text,
   View,
   Animated,
   Dimensions,
+  Vibration,
 } from "react-native";
 import { Button } from "react-native-elements";
+import colors from "../../constants/colors";
 import Colors from "../../constants/colors";
 
 import Navbar from "../../core/components/Navbar";
 
-const { width, height } = Dimensions.get("window");
+const { width } = Dimensions.get("window");
 const ITEM_SIZE = width * 0.42;
 const ITEM_SPACING = (width - ITEM_SIZE) / 2;
 const timers = [25, 50, 90];
@@ -23,71 +22,151 @@ const Focus = (props) => {
   const { navigation } = props;
 
   const scrollX = useRef(new Animated.Value(0)).current;
-  const [duration, setDuration] = useState(timers[0]);
+  const [intervalRef, setIntervalRef] = useState();
+  const [startTimer, setStartTimer] = useState(false);
+  const [timeLeft, setTimeLeft] = useState("");
+  const [pause, setPause] = useState(false);
   const [index, setIndex] = useState(0);
+  const [actualSeconds, setActualSeconds] = useState(0);
 
   useEffect(() => {
     scrollX.addListener(({ value }) => {
       const index = Math.round(value / width);
       setIndex(index);
-      setDuration(timers[index]);
-      console.log(timers[index], index);
     });
   });
 
+  const timerFunction = () => {
+    // Set the date we're counting down to
+    if (actualSeconds > 0)
+      var countDownDate = new Date().getTime() + actualSeconds * 1000;
+    else var countDownDate = new Date().getTime() + timers[index] * 60000;
+
+    // Update the count down every 1 second
+    setIntervalRef(
+      setInterval(function () {
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var hours = Math.floor(
+          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Output the result in an element with id="demo"
+
+        if (hours) {
+          setActualSeconds(hours * 3600 + minutes * 60 + seconds);
+          setTimeLeft(hours + "h " + minutes + "m " + seconds + "s ");
+        } else {
+          setActualSeconds(minutes * 60 + seconds);
+          setTimeLeft(minutes + "m " + seconds + "s ");
+        }
+
+        // If the count down is over, write some text
+        if (distance < 0) {
+          clearInterval(x);
+          setStartTimer(false);
+          Vibration.vibrate(10 * 1000); // 10 seconds
+        }
+      }, 1000)
+    );
+  };
+
+  useEffect(() => {
+    if (startTimer) {
+      timerFunction();
+    }
+  }, [startTimer]);
+
   return (
     <View style={styles.focusContainer}>
-      {/* <View style={styles.grid}>
-        <Text style={styles.heading}>Start a focus session</Text> */}
+      <Text style={styles.heading}>Start a focus session</Text>
 
-      <View
-        style={{
-          position: "absolute",
-          top: height / 3.3,
-          left: 0,
-          right: 0,
-          flex: 1,
-        }}
-      >
-        <Text style={styles.text}>{duration}</Text>
-        <Animated.FlatList
-          data={timers}
-          keyExtractor={(item) => item.toString()}
-          horizontal
-          bounces={false}
-          onScroll={Animated.event(
-            [{ nativeEvent: { contentOffset: { x: scrollX } } }],
-            { useNativeDriver: true }
-          )}
-          showsHorizontalScrollIndicator={false}
-          snapToInterval={ITEM_SIZE}
-          decelerationRate="slow"
-          pagingEnabled
-          onMomentumScrollEnd={(ev) => {
-            setDuration(timers[index]);
-          }}
-          style={{ flexGrow: 0 }}
-          contentContainerStyle={{
-            paddingHorizontal: ITEM_SPACING,
-          }}
-          renderItem={({ item }) => {
-            return (
-              <View
-                style={{
-                  width: ITEM_SIZE * 1.7,
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <Animated.Text style={[styles.text]}>{item}</Animated.Text>
-              </View>
-            );
-          }}
-        />
+      <View style={{ paddingTop: 200 }}>
+        {!startTimer ? (
+          <Animated.FlatList
+            data={timers}
+            keyExtractor={(item) => item.toString()}
+            horizontal
+            bounces={false}
+            onScroll={Animated.event(
+              [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+              { useNativeDriver: true }
+            )}
+            showsHorizontalScrollIndicator={false}
+            snapToInterval={ITEM_SIZE}
+            decelerationRate="slow"
+            pagingEnabled
+            contentContainerStyle={{
+              paddingHorizontal: ITEM_SPACING,
+            }}
+            renderItem={({ item }) => {
+              return (
+                <View
+                  style={{
+                    width: ITEM_SIZE * 2.3,
+                    marginRight: "-180px",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <Animated.Text style={[styles.text]}>{item}</Animated.Text>
+                </View>
+              );
+            }}
+          />
+        ) : (
+          <View>
+            <Text style={styles.timeLeft}>{timeLeft}</Text>
+          </View>
+        )}
       </View>
 
-      <Button title="Focus" buttonStyle={styles.focusButton} />
-      {/* </View> */}
+      <View style={styles.buttonContainer}>
+        {startTimer ? (
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            <Button
+              title="Cancel"
+              buttonStyle={styles.cancelButton}
+              onPress={() => {
+                setTimeLeft("");
+                setStartTimer(false);
+                clearInterval(intervalRef);
+                setActualSeconds(0);
+              }}
+            />
+
+            <Button
+              title={pause ? "Resume" : "Pause"}
+              buttonStyle={styles.pauseButton}
+              onPress={() => {
+                if (pause === false) {
+                  setPause(true);
+                  clearInterval(intervalRef);
+                } else {
+                  setPause(false);
+                  timerFunction();
+                }
+              }}
+            />
+          </View>
+        ) : (
+          <Button
+            title="Focus"
+            buttonStyle={styles.focusButton}
+            onPress={() => setStartTimer(true)}
+          />
+        )}
+      </View>
+
       <Navbar navigation={navigation} />
     </View>
   );
@@ -114,151 +193,38 @@ const styles = StyleSheet.create({
     paddingTop: 20,
   },
 
-  grid: {
-    height: "85%",
-    alignItems: "center",
-    justifyContent: "space-between",
+  buttonContainer: {
+    position: "fixed",
+    bottom: 100,
+    left: "50%",
+    transform: "translateX(-50%)",
   },
+
   text: {
     fontSize: ITEM_SIZE * 0.8,
     fontFamily: "Menlo",
     color: "#fff",
     fontWeight: "900",
   },
+
+  timeLeft: {
+    color: "#fff",
+    fontSize: 48,
+    textAlign: "center",
+  },
+
+  cancelButton: {
+    backgroundColor: "transparent",
+    width: 120,
+    marginRight: 10,
+  },
+
+  pauseButton: {
+    backgroundColor: "#111e1e",
+    borderRadius: "20px",
+    width: 120,
+    marginLeft: 10,
+  },
 });
 
 export default Focus;
-// import React, { useEffect, useState, useRef, useCallback } from "react";
-
-// import {
-//   Vibration,
-//   StatusBar,
-//   TextInput,
-//   Dimensions,
-//   Animated,
-//   TouchableOpacity,
-//   FlatList,
-//   Text,
-//   View,
-//   StyleSheet,
-// } from "react-native";
-
-// const { width, height } = Dimensions.get("window");
-// const colors = {
-//   black: "#323F4E",
-//   red: "#F76A6A",
-//   text: "#ffffff",
-// };
-
-// const timers = [25, 50, 90];
-// const ITEM_SIZE = width * 0.42;
-// const ITEM_SPACING = (width - ITEM_SIZE) / 2;
-
-// export default function Focus() {
-//   const scrollX = useRef(new Animated.Value(0)).current;
-//   const [duration, setDuration] = useState(timers[0]);
-//   const [index, setIndex] = useState(0);
-
-//   useEffect(() => {
-//     scrollX.addListener(({ value }) => {
-//       const index = Math.round(value / width);
-//       setIndex(index);
-//       setDuration(timers[index]);
-//       console.log(timers[index], index);
-//     });
-//   });
-
-//   return (
-//     <View style={styles.container}>
-//       <StatusBar hidden />
-//       <Animated.View
-//         style={[
-//           StyleSheet.absoluteFill,
-//           {
-//             height,
-//             width,
-//             backgroundColor: colors.black,
-//           },
-//         ]}
-//       />
-//       <Animated.View
-//         style={[
-//           StyleSheet.absoluteFillObject,
-//           {
-//             justifyContent: "flex-end",
-//             alignItems: "center",
-//             paddingBottom: 100,
-//           },
-//         ]}
-//       >
-//         <TouchableOpacity>
-//           <View style={styles.roundButton} />
-//         </TouchableOpacity>
-//       </Animated.View>
-//       <View
-//         style={{
-//           position: "absolute",
-//           top: height / 3.3,
-//           left: 0,
-//           right: 0,
-//           flex: 1,
-//         }}
-//       >
-//         <Text style={styles.text}>{duration}</Text>
-//         <Animated.FlatList
-//           data={timers}
-//           keyExtractor={(item) => item.toString()}
-//           horizontal
-//           bounces={false}
-//           onScroll={Animated.event(
-//             [{ nativeEvent: { contentOffset: { x: scrollX } } }],
-//             { useNativeDriver: true }
-//           )}
-//           showsHorizontalScrollIndicator={false}
-//           snapToInterval={ITEM_SIZE}
-//           decelerationRate="slow"
-//           pagingEnabled
-//           onMomentumScrollEnd={(ev) => {
-//             setDuration(timers[index]);
-//           }}
-//           style={{ flexGrow: 0 }}
-//           contentContainerStyle={{
-//             paddingHorizontal: ITEM_SPACING,
-//           }}
-//           renderItem={({ item, index }) => {
-//             return (
-//               <View
-//                 style={{
-//                   width: ITEM_SIZE * 1.7,
-//                   justifyContent: "center",
-//                   alignItems: "center",
-//                 }}
-//               >
-//                 <Animated.Text style={[styles.text]}>{item}</Animated.Text>
-//               </View>
-//             );
-//           }}
-//         />
-//       </View>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: colors.black,
-//   },
-//   roundButton: {
-//     width: 80,
-//     height: 80,
-//     borderRadius: 80,
-//     backgroundColor: colors.red,
-//   },
-//   text: {
-//     fontSize: ITEM_SIZE * 0.8,
-//     fontFamily: "Menlo",
-//     color: colors.text,
-//     fontWeight: "900",
-//   },
-// });
